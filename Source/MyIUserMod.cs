bde89a04fe4fd994fc86ea69442e11a896e1d2a6
Jake Anderson <barykade@umich.edu>
2015-04-29 16:42:04 -0400

    Add skeleton code to repo

---
+++ Source/MyIUserMod.cs
@@ -0,0 +1,25 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace ExampleNamespace
+{
+
+    public class ExampleMod: IUserMod
+    {
+
+        public string Name 
+        {
+            get { return "Example Mod"; }
+        }
+
+        public string Description 
+        {
+            get { return "This is an example mod"; }
+			
+        }
+    }
+
+    // Inherit interfaces and implement your mod logic here
+    // You can use as many files and subfolders as you wish to organise your code, as long
+    // as it remains located under the Source folder.
+}
