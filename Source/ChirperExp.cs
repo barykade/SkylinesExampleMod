706766b7338c171e30eca33bf597a68987812814
barykade <barykade@umich.edu>
2015-04-29 22:46:40 -0400

    Add ChirpLogger to files

---
+++ Source/ChirperExp.cs
@@ -0,0 +1,54 @@
+using ICities;
+using UnityEngine;
+
+using System.Collections.Generic;
+
+using ChirpLogger;
+
+namespace ExampleNamespace
+{
+  public class ChirperExp : IChirperExtension
+    {
+
+    public void OnCreated(IChirper chirper)
+    {
+      ChirpLog.Info("Chirp Logger Example Online");
+      ChirpLog.Debug(DumpAllGameObjects());
+    }
+
+    public void OnMessagesUpdated()
+    {
+
+    }
+
+    public void OnNewMessage(IChirperMessage message)
+    {
+      ChirpLog.Info("Chirper chirped: " + message.ToString());
+    }
+
+    public void OnReleased()
+    {
+      ChirpLog.Info("Chirp Logger Example Offline");
+      ChirpLog.Flush();
+    }
+
+    public void OnUpdate()
+    {
+      
+    }
+
+    public string DumpAllGameObjects()
+    {
+      List<GameObject> allObjects = new List<GameObject>( GameObject.FindObjectsOfType<GameObject>() );
+
+      string result = "";
+
+      foreach(GameObject obj in allObjects)
+      {
+        result += obj.name + "\n";
+      }
+
+      return result;
+    }
+  }
+}
--- Source/ExampleModMain.cs
+++ Source/ExampleModMain.cs
@@ -18,8 +18,4 @@ namespace ExampleNamespace
 			
         }
     }
-
-    // Inherit interfaces and implement your mod logic here
-    // You can use as many files and subfolders as you wish to organise your code, as long
-    // as it remains located under the Source folder.
 }
