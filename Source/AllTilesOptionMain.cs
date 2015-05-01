cf013821355e83e1a529bc8a946332a3c1112c10
barykade <barykade@umich.edu>
2015-05-01 00:32:16 -0400

    Example Mod Changed to All Tiles Purchaseable on Expansion

---
+++ Source/AllTilesOptionMain.cs
@@ -0,0 +1,16 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace AllTilesOptionNamespace
+{
+    public class AllTilesOptionMain: IUserMod
+    {
+        public string Name {
+            get { return "All Tiles Purchase Option"; }
+        }
+
+        public string Description {
+            get { return "When purchasing a new tile, the tile does not have to be adjacent to another to be purchased"; }
+        }
+    }
+}
---
+++ Source/AreasExt.cs
@@ -0,0 +1,28 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace AllTilesOptionNamespace
+{
+    public class AreasExt : IAreasExtension 
+    {
+        public void OnCreated(IAreas areas){
+
+        }
+
+        public void OnReleased(){
+
+        }
+
+        public bool OnCanUnlockArea(int x, int z, bool originalResult){
+            return true;
+        }
+
+        public int OnGetAreaPrice(uint ore, uint oil, uint forest, uint fertility, uint water, bool road, bool train, bool ship, bool plane, float landFlatness, int originalPrice){
+            return originalPrice;
+        }
+
+        public void OnUnlockArea(int x, int z){
+
+        }
+    }
+}
